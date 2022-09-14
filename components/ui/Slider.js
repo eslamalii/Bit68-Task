ee69f25dd8165dfac63381a65e011d1745dedd24
Eslam Ali <preslamali@hotmail.com>
2022-09-14 17:13:56 +0200

    Add Slider

---
+++ components/ui/Slider.js
@@ -0,0 +1,79 @@
+// import Swiper core and required modules
+import {
+  EffectCreative,
+  Autoplay,
+  EffectFade,
+  Navigation,
+  Pagination,
+} from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import {
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+} from "react-icons/md";
+import Image from "next/image";
+
+export default ({ data }) => {
+  return (
+    <div className="relative">
+      <Swiper
+        grabCursor={true}
+        effect={"creative"}
+        creativeEffect={{
+          prev: {
+            shadow: false,
+            translate: ["-10%", 0, -1],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        }}
+        modules={[EffectCreative, Autoplay, EffectFade, Navigation, Pagination]}
+        navigation={{
+          nextEl: ".swiper-next ",
+          prevEl: ".swiper-prev",
+        }}
+        pagination={{
+          el: ".swiper-pagination  ",
+          bulletActiveClass: ".swiper-pagination-bullet",
+        }}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        className="h-[200px] md:h-[400px] bg-[#F1F1F1] w-full"
+      >
+        {data.map((item, index) => (
+          <SwiperSlide key={index}>
+            <Image src={item.img} layout="fill" objectFit="contain" alt="" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <div className="swiper-next absolute top-1/2 mx-12 -right-0 z-10 flex cursor-pointer items-center justify-center">
+        <MdOutlineKeyboardArrowRight
+          size={30}
+          className="rounded-full border opacity-40 transition-all bg-white hover:bg-secondary-100 hover:opacity-100 md:h-10 md:w-10"
+        />
+      </div>
+      <div className="swiper-prev absolute top-1/2 mx-12 -left-0 z-10 flex cursor-pointer items-center justify-center">
+        <MdOutlineKeyboardArrowLeft
+          size={30}
+          className="rounded-full border opacity-40 transition-all bg-white hover:bg-secondary-100 hover:opacity-100 md:h-10 md:w-10"
+        />
+      </div>
+
+      <div className="swiper-pagination  absolute bottom-4 drop-shadow-lg p-1 flex  items-center justify-center rounded-lg left-1/2 z-10 cursor-pointer bg-white gap-3 "></div>
+
+      <div className="swiper-pagination-bullet active:!bg-[#1f58ac]"></div>
+    </div>
+  );
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from "../components/layout/Layout";
+import Slider from "../components/ui/Slider";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
